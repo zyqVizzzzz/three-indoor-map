b9e415aeaaf376069639d2e7799416f08502c6dd
zhongyuqiang <zhongyuqiang@mail.com>
2020-07-10 15:50:16 +0800

    feat: add resize method.

--- src/Map/index.js
+++ src/Map/index.js
@@ -8,6 +8,9 @@ function Map() {
   let camera = null;
   let renderer = null;
   let controls = null;
+  const container = document.body;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
   useEffect(() => {
     _init();
@@ -17,16 +20,18 @@ function Map() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(
       25,
-      window.innerWidth / window.innerHeight,
+      width / height,
       0.1,
       2000
     );
     camera.position.set(360, 250, 700);
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
 
     controls = new OrbitControls(camera, renderer.domElement);
-    document.body.appendChild(renderer.domElement);
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', resize, false)
 
     loadMap("./data/map1.json");
     createLight();
@@ -51,10 +56,20 @@ function Map() {
 
   //set up the lights
   const createLight = () => {
-    let light = new THREE.AmbientLight(0xffffff, 0.2)
-    scene.add(light)
-    light = new THREE.HemisphereLight(0xffffff, 0x000000, 0.9)
-    scene.add(light)
+    let light = new THREE.AmbientLight(0xffffff, 0.2);
+    scene.add(light);
+    light = new THREE.HemisphereLight(0xffffff, 0x000000, 0.9);
+    scene.add(light);
+  };
+
+  const resize = () => {
+    let width = window.innerWidth;
+    let height = window.innerHeight
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+    controls.viewChanged = true;
   };
 
   return <div id="canvas" />;
